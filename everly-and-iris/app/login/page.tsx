cee6157bef0a303957012b78e814d08626120b31
github username <github email address>
2026-09-14 15:19:15 +0200

    updated home page

--- everly-and-iris/app/login/page.tsx
+++ everly-and-iris/app/login/page.tsx
@@ -4,7 +4,8 @@ export default function LoginPage() {
 	return (
 		<>
 			<Image
-				src={"/almond"}
+				//              src='https://seligmar.github.io/robert-a-selig/route-map-vertical.jpg'
+				src={"/almond.jpeg"}
 				alt={"Almond Blossom, February 1890. Van Gogh Museum, Amsterdam"}
 				width={100}
 				height={100}
