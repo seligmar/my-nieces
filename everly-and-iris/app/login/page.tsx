ea6cfe091525ff7fd80808b4932abe5e19b85465
github username <github email address>
2026-09-14 19:16:33 +0200

    add toast alert

--- everly-and-iris/app/login/page.tsx
+++ everly-and-iris/app/login/page.tsx
@@ -1,22 +1,49 @@
 "use client";
-import LoginForm from "./loginForm";
+import { useState } from "react";
 import Image from "next/image";
+import LoginForm from "./loginForm";
+import ToastAlert from "./toastAlert";
 import { username, password } from "./username";
-\import "./login.css";
+import "./login.css";
 
 export default function LoginPage() {
+	const [open, openToast] = useState<boolean>(false);
+	const [status, setStatus] = useState<string | undefined>(undefined);
+
 	function onSubmit(e: any) {
 		e.preventDefault();
 		const username1 = e.target["username"].value;
 		const password1 = e.target["password"].value;
-		if (username1 !== username || password1 !== password) console.log("nope");
-		else {
-			console.log("hooray!");
+		if (username1 !== username || password1 !== password) {
+			openToast(true);
+			setStatus("failure");
+			setTimeout(function () {
+				openToast(false);
+				setStatus(undefined);
+			}, 7000);
+		} else {
+			openToast(true);
+			setStatus("success");
+			//  form.reset();
+			setTimeout(function () {
+				openToast(false);
+				setStatus(undefined);
+			}, 7000);
 		}
+	}
+
+	function closeToast() {
+		setStatus(undefined);
+		openToast(false);
 	}
 
 	return (
 		<div className="almond-blossom login-background">
+			<ToastAlert
+				closeToast={closeToast}
+				open={open}
+				status={status}
+			/>
 			<Image
 				// src='https://seligmar.github.io/robert-a-selig/route-map-vertical.jpg'
 				src={"/blossoms.png"}
