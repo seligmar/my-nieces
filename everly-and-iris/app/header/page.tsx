4fae17b09f1eaa1637588e76addfcd91b97ca867
github username <github email address>
2026-09-22 11:00:01 +0200

    added colours to header

--- everly-and-iris/app/header/page.tsx
+++ everly-and-iris/app/header/page.tsx
@@ -1,5 +1,5 @@
 import "./header.css";
 
 export default function Header() {
-	return <div>header</div>;
+	return <div className="header">header</div>;
 }
--- everly-and-iris/app/home/page.tsx
+++ everly-and-iris/app/home/page.tsx
@@ -6,24 +6,25 @@ export default function HomePage() {
 	return (
 		<>
 			<Header />
-			<Image
-				// src='https://seligmar.github.io/robert-a-selig/route-map-vertical.jpg'
-				src={"/iris.png"}
-				alt={"Almond Blossom, February 1890. Van Gogh Museum, Amsterdam"}
-				width={600}
-				height={600}
-				//	layout="fill"
-				//	objectFit="contain"
-			/>
-			<Image // src='https://seligmar.github.io/robert-a-selig/route-map-vertical.jpg'
-				src={"/forest.png"}
-				alt={"Almond Blossom, February 1890. Van Gogh Museum, Amsterdam"}
-				width={600}
-				height={600}
-				//layout="fill"
-				//	objectFit="contain"
-			/>
-			<div>home</div>
+			<div className="homepage-background">
+				<Image
+					// src='https://seligmar.github.io/robert-a-selig/route-map-vertical.jpg'
+					src={"/iris.png"}
+					alt={"Almond Blossom, February 1890. Van Gogh Museum, Amsterdam"}
+					width={600}
+					height={600}
+					//	layout="fill"
+					//	objectFit="contain"
+				/>
+				<Image // src='https://seligmar.github.io/robert-a-selig/route-map-vertical.jpg'
+					src={"/forest.png"}
+					alt={"Almond Blossom, February 1890. Van Gogh Museum, Amsterdam"}
+					width={600}
+					height={600}
+					//layout="fill"
+					//	objectFit="contain"
+				/>
+			</div>
 		</>
 	);
 }
