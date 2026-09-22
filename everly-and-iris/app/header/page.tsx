ce3c27b2c200ee5a3246199345e633a32bad71fb
github username <github email address>
2026-09-22 11:05:15 +0200

    added colours to header

--- everly-and-iris/app/header/page.tsx
+++ everly-and-iris/app/header/page.tsx
@@ -1,5 +1,11 @@
 import "./header.css";
 
 export default function Header() {
-	return <div className="header">header</div>;
+	return (
+		<div className="header">
+			<div className="iris">Iris</div>
+			<div>&</div>
+			<div className="everly">Everly</div>
+		</div>
+	);
 }
